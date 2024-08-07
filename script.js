85f95d5af018f82aecc004572479d63f17977a1c
mohammed kaif <mkaif555123@gmail.com>
2024-08-07 23:53:08 +0500

    !00% WOrk Done

--- script.js
+++ script.js
@@ -19,22 +19,46 @@ const getMovie = () => {
             .then(resp => resp.json())
             .then((data) => {
                 console.log(data);
-                result.innerHTML = `
-                <div class="info">
-                  <img src="${data.Poster}" alt=""> </img>
-                  <div> 
-                  <h2> ${data.Title} </h2>
-                  <div class="rating">
-                  <img src="pngwing.com (5).png" alt=""> </img>
-                  <h4> ${data.imdbRating} </h4>
-                  </div>
-                  </div>
-                </div> 
+                if(data.Response){
+
+                    result.innerHTML = `
+                    <div class="info">
+                    <img class="poster" src="${data.Poster}" alt=""> </img>
+                    <div> 
+                    <h2> ${data.Title} </h2>
+                    <div class="rating">
+                    <img src="pngwing.com (5).png" alt=""> </img>
+                    <h4> ${data.imdbRating} </h4>
+                    </div>
+                    <div class="details">
+                    <span>${data.Rated}</span>
+                    <span>${data.Year}</span>
+                    <span>${data.Runtime}</span>
+                    </div>
+                    <div class="genre">
+                    <div>${data.Genre.split(",").join("</div> <div>")}</div>
+                    </div>
+                    </div>
+                    </div> 
+                    
+                    <h3>Plot:</h3>
+                    <p>${data.Plot}</p>
+                    <h3>Cast:</h3>
+                    <P>${data.Actors}</P>
+                    `
+                    searchButton.innerText = "Search"
+                }
+
+                else[
+                    result.innerHTML = `<h3 class="msg">${data.Error}</h3>`
+                ]
+                })
+                .catch(() =>{
+                    result.innerHTML = `<h3 class="msg">Error Occured</h3>`
+                    searchButton.innerText = "Search"
+                })
                 
-                `
-                searchButton.innerText = "Search"
-            })
-    }
+            }
 
 };
 
